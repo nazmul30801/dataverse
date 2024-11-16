8afeffe91c26586a35b06cb93da906784ee688c8
Nazmul <nazmul30801@gmail.com>
2024-11-16 23:39:25 +0600

    Added App list in Home page

--- index.php
+++ index.php
@@ -1,61 +1,34 @@
+<?php 
+
+$root_dir = "";
+$page_id = "home";
+
+
+
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <?php require "meta_links.php"; ?>
-    <title>Dataverse</title>
-    <style>
-        .container {
-            width: 100%;
-            height: 100vh;
-            display: flex;
-            align-items: center;
-            justify-content: center;
-        }
-
-        hr {
-            margin: 4rem 0;
-            background-color: #5e5e5e;
-        }
-
-        .logo {
-            display: block;
-            width: 40rem;
-        }
-
-        .text {
-            font-family: monospace;
-            font-size: 2rem;
-            text-align: center;
-            padding: 5rem 1rem;
-            background-color: #e6e6e6;
-            border-radius: 2rem;
-            color: #f00;
-            box-shadow: inset #7f7f7fa1 2px 2px 10px 0;
-        }
-    </style>
+    <title><?php echo $title; ?></title>
 </head>
 
 <body>
     <!-- Body - Header -->
-    <?php require "header.php"; ?>
+    <?php require $root_dir."header.php"; ?>
 
     <!-- Main Body  -->
-    <main>
-        <div class="container">
-            <div class="display">
-                <img class="logo" src="/img/text-logo.png" alt="Dataverse">
-                <hr>
-                <div class="text">Site is under construct</div>
-            </div>
-        </div>
-    </main>
+    <?php require $root_dir."main.php"; ?>
 
     <!-- Body - Footer -->
-    <?php require "footer.php"; ?>
+    <?php require $root_dir."footer.php"; ?>
 
     <!-- End Scripts -->
-    <?php require "end_scripts.php"; ?>
+    <?php require $root_dir."end_scripts.php"; ?>
 </body>
 
 </html>
---
+++ main.php
@@ -0,0 +1,12 @@
+<section id="apps-list">
+    <div class="container p-5 d-flex justify-content-center">
+        <div class="app-box">
+            <div class="app-box-title display-1">Apps</div>
+            <div class="app-box-body d-flex flex-wrap justify-content-center">
+                <a href="/identity" class="app">Identity</a>
+                <a href="/caller_id/" class="app">Caller ID</a>
+                <a href="/shekor/" class="app">Shekor</a>
+            </div>
+        </div>
+    </div>
+</section>
