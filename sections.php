ba04a92bcc49e1fa7a98da35df08ef5e067bf495
Nazmul <nazmul30801@gmail.com>
2024-12-10 04:27:38 +0600

    removes extra images and move favicon to img folder

--- sections.php
+++ sections.php
@@ -5,7 +5,7 @@ function meta_links()
 	$meta_links = <<<HTML
 		<meta charset="UTF-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-		<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+		<link rel="shortcut icon" href="/img/favicon.ico" type="image/x-icon" />
 		<link rel="stylesheet" href="/assets/fontawesome/css/all.min.css">
 		<link rel="stylesheet" href="/assets/bootstrap/css/bootstrap.min.css">
 		<link rel="stylesheet" href="assets/custom/css/style.css" />
